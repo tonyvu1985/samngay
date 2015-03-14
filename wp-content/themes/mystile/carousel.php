1acd4593e01467e2a729cae26af0d699fb418931
Tony Vu <vuductrung2003@gmail.com>
2015-03-14 06:27:30 -0400

    banner 3

--- wp-content/themes/mystile/carousel.php
+++ wp-content/themes/mystile/carousel.php
@@ -3,12 +3,13 @@
   <ol class="carousel-indicators">
     <li data-target="#carousel-samngay" data-slide-to="0" class="active"></li>
     <li data-target="#carousel-samngay" data-slide-to="1"></li>
+    <li data-target="#carousel-samngay" data-slide-to="2"></li>
   </ol>
 
   <!-- Wrapper for slides -->
   <div class="carousel-inner" role="listbox">
     <div class="item active">
-      <img src="<?php echo get_bloginfo('url') . '/wp-content/uploads/2015/03/bnSukin.png' ?>" alt="">
+      <img src="<?php echo get_bloginfo('url') . '/wp-content/uploads/2015/03/bnSukin.png' ?>" alt="Sukin">
        <div class="carousel-caption">
             <h3>Sukin</h3>
             <p>Mặt nạ, kem dưỡng ẩm cho da mặt, sữa rửa mặt...</p>
@@ -16,11 +17,19 @@
       </div>
     </div>
     <div class="item">
-      <img src="<?php echo get_bloginfo('url') . '/wp-content/uploads/2015/03/bnNeutrogena-Naturals.png' ?>" alt="">
+      <img src="<?php echo get_bloginfo('url') . '/wp-content/uploads/2015/03/bnNeutrogena-Naturals.png' ?>" alt="Neutrogena Naturals">
       <div class="carousel-caption">
 		<h3>Neutrogena Naturals</h3>
 		<p>Sữa rửa mặt, kem dưỡng ẩm chống nết nhăn ban đêm, kem chống nắng...</p>
 		<a href="<?php echo  get_bloginfo('url') . '/product-category/cham-soc-da/?filter_nhan-hieu=61' ?>" class="btn btn-info">Xem chi tiết</a>
+      </div>
+	</div>
+    <div class="item">
+      <img src="<?php echo get_bloginfo('url') . '/wp-content/uploads/2015/03/bnNude.png' ?>" alt="Nude by nature">
+      <div class="carousel-caption">
+		<h3>Dòng sản phẩm son môi</h3>
+		<p>Son môi nước, son dưỡng môi, son môi khoáng chất ...</p>
+		<a href="<?php echo  get_bloginfo('url') . '/product-category/my-pham/moi/' ?>" class="btn btn-info">Xem chi tiết</a>
       </div>
     </div>
   </div>
