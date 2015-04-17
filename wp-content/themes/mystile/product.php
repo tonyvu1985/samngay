c3a15827d63e5f8070dab71fe364666586572f47
tony vu <tony.vu@mytraining.net>
2015-04-16 21:11:44 -0400

    style homepage

--- wp-content/themes/mystile/product.php
+++ wp-content/themes/mystile/product.php
@@ -6,12 +6,12 @@ function get_product_category_by_id($cat_Ids) {
 	foreach($cat_Ids as $id){
 		$term = get_term_by('id', $id, 'product_cat', 'ARRAY_A' );
 		echo '<hr/ style="clear:both">';
-		echo '<div class="col-xs-12">';
-		echo 	'<div class="col-md-8">';
+		echo '<div class="row">';
+		echo 	'<div class="col-md-8 col-xs-12">';
 		echo 		'<a href="' . get_bloginfo('url') . '/product-category/' . $term['slug'] . '"><span class="danhmuc">DM</span><h1 class="cat">' . $term['name']. '</h1></a>';
 		echo	'</div>';
-		echo	'<div class="col-md-4 text-right">';
-		echo		'<a class="btn btn-default btn-xemtatca" href="' . get_bloginfo('url') . '/product-category/' . $term['slug'] . '">Xem tất cả</a>';
+		echo	'<div class="col-md-4 col-xs-12 text-right xemtatca">';
+		echo		'<a class="btn btn-default" href="' . get_bloginfo('url') . '/product-category/' . $term['slug'] . '">Xem tất cả</a>';
 		echo	'</div>';
 		echo '</div>';
 		echo do_shortcode('[product_category category="' . $term['slug']  . '" per_page="8" order="asc"]');
